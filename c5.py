65b05716d8dc4d66a6e43c697091ca4814404c9d
Mosnio <130111318+Mosnio@users.noreply.github.com>
2023-11-30 15:21:09 +0530

    Create c5.py

---
+++ c5.py
@@ -0,0 +1,18 @@
+{
+    "Username": "Ghuavuvia111178@gmail.com",
+    "Password": "Ghuavuerb@252",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:95.0) Gecko/20100101 Firefox/95.0",
+    "Multibot Key": "5Z4sWvudz6Jjfw2nK10YcOFkRbQVMU",
+    "Auto Wd": {
+        "Toggle": "on",
+        "Faucetpay Btc": "16VawnGQeN5eGfLPndh94uR856LqNEup47"
+    },
+    "Xevil Key": "Iy4FqxuAw9nSpH9ZdMkENmZt4anH7q8V",
+    "Live Roll": {
+        "Toggle": "on",
+        "Min/Max": [
+            9994,
+            9999
+        ]
+    }
+}
